ac6e5cef36537478176c8fc3af0a0ba2347553a4
pranoti <pranotikadam96@gmail.com>
2024-05-31 02:14:18 +0530

    models created

---
+++ backend/config/dbConnect.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(process.env.DBNAME, process.env.USERNAME, process.env.PASSWORD, {
+  host: process.env.HOST,
+  port: process.env.DBPORT,
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+    connectTimeout: 86400
+  },
+});
+
+module.exports = sequelize;
